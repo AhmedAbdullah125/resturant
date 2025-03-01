94b9ed5911e10b7578234af7a7e41010ff9f485b
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2025-03-01 10:31:35 +0200

    update

--- src/components/home/Gallery.jsx
+++ src/components/home/Gallery.jsx
@@ -1,27 +1,20 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import BlurFade from '../ui/blur-fade';
-
 import Marquee from '../ui/marquee';
 import { cn } from '@/lib/utils';
-
-import img1 from '/public/grid1.png';
-import img2 from '/public/grid2.png';
-import img3 from '/public/grid3.png';
-import img4 from '/public/grid4.png';
-import img5 from '/public/grid5.png';
-import img6 from '/public/grid6.png';
-import img7 from '/public/grid7.png';
-import img8 from '/public/grid8.png';
-import img9 from '/public/grid9.png';
-import img10 from '/public/grid10.png';
-import img11 from '/public/grid11.png';
-
 import Link from 'next/link';
+import axios from 'axios';
+import { API_BASE_URL } from '@/lib/apiConfig';
+import Loading from '@/app/loading';
+import parse from 'html-react-parser';
 
 export default function Gallery() {
     const [language, setLanguage] = useState('en');  // Default language is 'en'
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
     useEffect(() => {
         if (typeof window !== 'undefined') {
             // Define the headers with the selected language
@@ -29,21 +22,25 @@ export default function Gallery() {
             const headers = {
                 lang: localStorage.getItem('lang'), // Change language dynamically based on state
             };
+            axios.get(`${API_BASE_URL}/lava/offers`
+                , {
+                    headers: headers,
+                }
+            )
+                .then(response => {
+                    setData(response.data.data);  // Set the response data to state
+                    setLoading(false);  // Set loading to false
+                })
+                .catch(error => {
+                    setError(error);  // Handle any errors
+                    console.error('Error fetching data:', error);
+                    setLoading(false)
+                });
         }
     }, []);
-    let imgs =
-        [
-            { url: img3, category: 'photos', name: "burger" },
-            { url: img5, category: 'photos', name: "burger" },
-            { url: img2, category: 'photos', name: "burger" },
-            { url: img1, category: 'photos', name: "burger" },
-            { url: img4, category: 'photos', name: "burger" },
-            { url: img5, category: 'photos', name: "burger" },
-            { url: img6, category: 'photos', name: "burger" },
 
-        ]
     const ReviewCard = ({
-        url, name
+        image, name , discount,description , priceAfterDiscount ,priceBeforeDiscount
     }) => {
         return (
             <figure className={cn()} >
@@ -51,13 +48,15 @@ export default function Gallery() {
                     <figure className='relative g-imgs-cont'>
                         <div className="offered-meal-overlay">
                             <h3 className='offered-meal-title'>{name}</h3>
-                            <h5 className='offered-meal-text'>{language === 'en' ? 'Order Now and Get 20% Off' : 'اطلب الان وحصل على 20% خصم'}</h5>
+                            <p>{parse(description)}</p>
+                            <h6 className='price'><span className='new-price'>{priceAfterDiscount}</span> <span className='old-price'>{priceBeforeDiscount}</span></h6>
+                            <h5 className='offered-meal-text'>{language === 'en' ? `Order Now and Get ${discount}% Off` : `اطلب الان وحصل على ${discount}% خصم`}</h5>
                             <Link href={'/meal'} className="button">
                                 <span >{language === 'en' ? 'Order Now' : 'اطلب الان'}</span>
                                 <i className="fa-solid fa-arrow-right"></i>
                             </Link>
                         </div>
-                        <Image src={url} alt="Mazar" width={200} height={200} className="mb-4 size-full rounded-3xl object-contain" />
+                        <Image src={image} alt="Mazar" width={200} height={200} className="mb-4 size-full rounded-3xl object-contain" />
                     </figure>
                 </div>
             </figure>
@@ -86,7 +85,7 @@ export default function Gallery() {
                         ))}
                     </div> */}
                     <Marquee pauseOnHover className="[--duration:20s]">
-                        {imgs.map((review, index) => (
+                        {data.map((review, index) => (
                             <ReviewCard key={index} {...review} />
                         ))}
                     </Marquee>
